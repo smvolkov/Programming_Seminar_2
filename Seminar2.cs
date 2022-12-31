54204f2e66ad6e5e56078a7d7019192ca32c9a47
Semion Volkov <semen_volkov@mail.ru>
2022-12-31 10:33:23 +0500

    Task2 added

--- Seminar2.cs
+++ Seminar2.cs
@@ -6,11 +6,27 @@
 
 void Task1()
 {
-    Console.WriteLine("Введите число: ");
+    Console.WriteLine("Введите трехзначное число: ");
     string number = Console.ReadLine();
 
     Console.WriteLine(number[1]);
 } 
 
-Task1();
+//Task1();
 
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+void Task2()
+{
+    Console.WriteLine("Введите число: ");
+    string N = Console.ReadLine();
+
+    if (N.Length > 2) Console.WriteLine(N[2]);
+    else Console.WriteLine("Третьей цифры нет");
+}
+
+Task2();
